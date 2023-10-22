4d3d0336a4c7833de8e8529077dc3473e9ab2e70
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-10-22 16:27:11 +0400

    Wrote 1 programm unfinished

--- Program.cs
+++ Program.cs
@@ -2,3 +2,12 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 100;
+int secondDigit = number / 10;
+int result = secondDigit - (firstDigit * 10);
+
+Console.WriteLine($" {number} -> {result}");
