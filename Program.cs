0d4a73aefcee9b5980bb9b474b693001b001cf93
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-10-23 12:30:46 +0300

    Wrote 3 programm

--- Program.cs
+++ Program.cs
@@ -25,19 +25,36 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if(number >= 100)
-{
-    while(number >= 1000)
-    {
-        number /= 10;
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number >= 100)
+// {
+//     while(number >= 1000)
+//     {
+//         number /= 10;
        
-    }
-    int thirdDigit = number % 10;
-    Console.WriteLine($"{number} -> {thirdDigit}");
-}
-else
-{
-    Console.WriteLine("Третьей цифры нет");
-}
+//     }
+//     int thirdDigit = number % 10;
+//     Console.WriteLine($"{number} -> {thirdDigit}");
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.Write("Введите цифру дня недели, чтобы узнать является ли он выходным : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number == 7 || number == 6)
+// {
+//     Console.WriteLine($"{number} -> да");
+// }
+// else
+// {
+//     Console.WriteLine("нет");
+// }
+
