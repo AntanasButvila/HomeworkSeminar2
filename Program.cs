0dac8e1ccdae9a0995c17373830e8a0c018cc6c7
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-10-23 10:51:30 +0300

    Wrote first programm

--- Program.cs
+++ Program.cs
@@ -5,9 +5,16 @@
 
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
+if(number < 1000 && number > 99 )
+{
+  
 int firstDigit = number / 100;
 int secondDigit = number / 10;
 int result = secondDigit - (firstDigit * 10);
 
 Console.WriteLine($" {number} -> {result}");
+}
+else
+{
+    Console.Write("Вы ввели не трехзначное число!");
+}
