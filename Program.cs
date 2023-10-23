22e0b959bee99ff851ea48ef94e3e3d9d3fae40d
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-10-23 11:43:45 +0300

    Wrote 2 programm

--- Program.cs
+++ Program.cs
@@ -24,3 +24,20 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 100)
+{
+    while(number >= 1000)
+    {
+        number /= 10;
+       
+    }
+    int thirdDigit = number % 10;
+    Console.WriteLine($"{number} -> {thirdDigit}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
